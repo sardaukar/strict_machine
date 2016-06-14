db4623aa7a8b619620beedd7c0147954b4b2280f
Bruno Antunes <bruno@thoughtbot.com>
2016-06-14 15:48:11 +0100

    only prepend Initializer on classes with mounts

--- lib/strict_machine.rb
+++ lib/strict_machine.rb
@@ -1,7 +1,7 @@
 require_relative "ext/object"
 
 module StrictMachine
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
 
   class << self; attr_accessor :list; end
 end
--- lib/strict_machine/mount_state_machine/initializer.rb
+++ lib/strict_machine/mount_state_machine/initializer.rb
@@ -2,16 +2,18 @@ module StrictMachine
   module MountStateMachine
     module Initializer
       def initialize
-        @state_machine = self.class.strict_machine_class.new
-        @state_machine.mounted_on = self
+        if self.class.respond_to?(:strict_machine_class)
+          @state_machine = self.class.strict_machine_class.new
+          @state_machine.mounted_on = self
 
-        options = self.class.strict_machine_options
-        state_attr = options.fetch(:state, :status)
-        @state_machine.state_attr = state_attr
+          options = self.class.strict_machine_options
+          state_attr = options.fetch(:state, :status)
+          @state_machine.state_attr = state_attr
 
-        @state_machine.boot!
+          @state_machine.boot!
 
-        super
+          super
+        end
       end
     end
   end
